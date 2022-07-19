e380f50a0729f9ddacb9df306a30fa8d09cf3d55
stanleyboy2003s <stanleyboy2003s@hotmail.com>
2022-07-20 01:35:27 +0800

    Add project files.

---
+++ HotelListening.API/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//開放API的跨域特性，允許任意來源且不限定header以及method
+builder.Services.AddCors( options => 
+    options.AddPolicy("AllowAll",
+    builder => builder.AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod())
+    );
+
+//使用Serilog讀取配置文件(appsettings.json)來進行記錄並輸出到控制台
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
